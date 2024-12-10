fe6fea7ab85012fef75529dbfd076381a5b03380
Artem Kriukov <art.kriukov@gmail.com>
2024-12-10 15:58:50 +0300

    Реализована логика приложения

--- GuessTheNumber/ViewController.swift
+++ GuessTheNumber/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private weak var userNumberUITextView: UITextField!
+    @IBOutlet private weak var checkNumberUIButton: UIButton!
+    @IBOutlet private weak var resultUILabel: UILabel!
+    
+    var randomNumber = Int.random(in: 1...100)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkNumberUIButton.layer.cornerRadius = 10
+        print(randomNumber)
     }
 
+    @IBAction func didTapResultButton(_ sender: UIButton) {
+        let userNumber = userNumberUITextView.text ?? ""
+        
+        if String(randomNumber) == userNumber {
+            resultUILabel.text = "Вы угадали число"
+        } else if userNumber < String(randomNumber) {
+            resultUILabel.text = "Ваше число меньше, чем загадал компьютер"
+        } else if userNumber > String(randomNumber){
+            resultUILabel.text = "Ваше число больше, чем загадал компьютер"
+        }
+    }
 
 }
 
